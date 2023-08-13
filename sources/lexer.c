1bd63d6f3e37e6d96944d66d7fb3515bd4c16911
Marc Lindenmaier <mlindenm@2-F-8.42heilbronn.de>
2023-08-14 01:41:27 +0200

    started lexer. got tired

--- sources/lexer.c
+++ sources/lexer.c
@@ -6,50 +6,67 @@
 /*   By: mlindenm <mlindenm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 18:32:39 by mlindenm          #+#    #+#             */
-/*   Updated: 2023/08/13 22:09:26 by mlindenm         ###   ########.fr       */
+/*   Updated: 2023/08/14 01:25:08 by mlindenm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 #include <stdio.h> // printf();
 
-// void	check_arg(t_stack_ptr *p, int argc, char *argv[])
-// {
-// 	int		i;
-// 	int		j;
+static int	check_string(char *str)
+{
+	int	i;
 
-// 	i = 1;
-// 	j = 0;
-// 	p->ptrptr = NULL;
-// 	while (i < argc)
-// 	{
-// 		p->ptrptr = ft_split(argv[i++], ' ');
-// 		if (p->ptrptr == NULL)
-// 			error_data(p, "split failed!\n");
-// 		while (*(p->ptrptr + j) != NULL)
-// 		{
-// 			if (!check_string(*(p->ptrptr + j)))
-// 				error_data(p, "invalid characters as arguments!\n");
-// 			j++;
-// 		}
-// 		j = 0;
-// 		free_ptrptr(p);
-// 	}
-// }
+	i = 0;
+	if (*(str) == '+' || *(str) == '-')
+		i++;
+	if (*(str + i) < '0' || *(str + i) > '9')
+		return (0);
+	while (*(str + i) != '\0')
+	{
+		if (*(str + i) >= '0' && *(str + i) <= '9')
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
+
+void	split_input(char *input)
+{
+	int		j;
+
+	j = 0;
+	get_data()->tokens = NULL;
+	get_data()->tokens = ft_split(input, ' ');
+	if (get_data()->tokens == NULL)
+		error("split failed!\n");
+	while (*(get_data()->tokens + j) != NULL)
+	{
+		if (!check_string(*(get_data()->tokens + j)))
+			error("invalid characters as arguments!\n");
+		j++;
+	}
+	j = 0;
+	// free_ptrptr(p);
+}
 
 void	lexer(char *input)
 {
-	char	**test;
 	int		i;
 
-	test = ft_split(input, ' ');
-	printf("%s \n", test[0]);
+	get_data()->tokens = ft_split(input, ' ');
 	i = 0;
-	while (test[i] != NULL)
+	while (get_data()->tokens[i] != NULL)
+		printf("%d\n", ++i);
+	// split_input(input);
+	printf("%s \n", get_data()->tokens[0]);
+	i = 0;
+	while (get_data()->tokens[i] != NULL)
 	{
-		if (test[i] != NULL)
-			free(test[i++]);
+		if (get_data()->tokens[i] != NULL)
+			free(get_data()->tokens[i++]);
 	}
-	if (test != NULL)
-		free(test);
+	if (get_data()->tokens != NULL)
+		free(get_data()->tokens);
 }
