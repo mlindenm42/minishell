e5d0b54c3e11d2f8b94c120284ebf6e8175fa1dd
Marc Lindenmaier <mlindenm@2-F-8.42heilbronn.de>
2023-10-17 12:47:45 +0200

    quot_utils back to where it worked

--- sources/builtins/env_utils.c
+++ sources/builtins/env_utils.c
@@ -6,7 +6,7 @@
 /*   By: mlindenm <mlindenm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 06:14:54 by mlindenm          #+#    #+#             */
-/*   Updated: 2023/10/17 08:11:54 by mlindenm         ###   ########.fr       */
+/*   Updated: 2023/10/17 12:43:44 by mlindenm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,70 +58,102 @@ static void	shiftenvs(char *var, char *envp[], int shift, char *end)
 	}
 }
 
-// void replace_var(char *vname, char *val, char *envp[])
-// {
-// 	int	len;
-// 	char *start;
-// 	char *dst;
-// 	char *end;
-// 	char	*env_end;
-// 	int	n;
-
-// 	len = varlen(vname); //vname malloced needed only for length
-// 	start = getvarstart(vname, envp, len);//p0
-// 	end = start + ft_strlen(start);// end of old var start of after end + 1  p2
-// 	if (val != NULL)
-// 		len = len + ft_strlen(val) + 1;//new len
-// 	dst = start + len + 1;//place where after is shifted
-// 	if (ft_strchr(start, '=') == NULL)
-// 	 	*end = '=';
-// 	if (val == NULL)
-// 		*(dst - 1) = '\0';
-// 	env_end = getenvmem_end(envp);
-// 	if (env_end != end)
-// 		ft_memmove(dst, end + 1, env_end - end);
-// 	else
-// 	{
-// 		n = arr_len(envp);
-// 		envp[n - 2] = start;
-// 	}
-// 	if (dst - end - 1 < 0)
-// 		ft_bzero(env_end + (dst - end) - 1, end + 1 - dst);
-// 	if (val != NULL)
-// 		ft_strlcpy(start + varlen(vname) + 1, val, ft_strlen(val) + 1);
-// 	if (env_end != end)
-// 		shiftenvs(start, envp, len - (end - start), env_end);
-// }
-
-void	replace_var(char *v, char *val, char *e[])
+//if a variable exists returns the pointer
+//to its start
+//vname only first l chars!!!
+char *getvarstart(char *vname, char *envp[], int l)
 {
-	int		len;
-	char	*dst;
-	char	*end;
-	char	*env_end;
+	int	i;
 
-	len = 0;
-	end = var(v, e, vlen(v)) + ft_strlen(var(v, e, vlen(v)));
+	i = 0;
+	while (envp[i] != NULL)
+	{
+		if (ft_strncmp(envp[i], vname, l) == 0
+			&& (envp[i][l] == '=' || envp[i][l] == '\0' ))
+			return (envp[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+//gets length of a variable from an argument)
+//var1=test var2
+int 	varlen(char *var)
+{
+	int	i;
+
+	i = 0;
+	while (var[i] != '=' && var[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+void replace_var(char *vname, char *val, char *envp[])
+{
+	int	len;
+	char *start;
+	char *dst;
+	char *end;
+	char	*env_end;
+	int	n;
+
+	len = varlen(vname); //vname malloced needed only for length
+	start = getvarstart(vname, envp, len);//p0
+	end = start + ft_strlen(start);// end of old var start of after end + 1  p2
 	if (val != NULL)
-		len = vlen(v) + ft_strlen(val) + 1;
-	dst = var(v, e, vlen(v)) + len + 1;
-	if (ft_strchr(var(v, e, vlen(v)), '=') == NULL)
-		*end = '=';
+		len = len + ft_strlen(val) + 1;//new len
+	dst = start + len + 1;//place where after is shifted
+	if (ft_strchr(start, '=') == NULL)
+	 	*end = '=';
 	if (val == NULL)
 		*(dst - 1) = '\0';
-	env_end = getenvmem_end(e);
+	env_end = getenvmem_end(envp);
 	if (env_end != end)
 		ft_memmove(dst, end + 1, env_end - end);
 	else
-		e[arr_len(e) - 2] = var(v, e, vlen(v));
+	{
+		n = arr_len(envp);
+		envp[n - 2] = start;
+	}
 	if (dst - end - 1 < 0)
 		ft_bzero(env_end + (dst - end) - 1, end + 1 - dst);
 	if (val != NULL)
-		ft_strlcpy(var(v, e, vlen(v)) + vlen(v) + 1, val, ft_strlen(val) + 1);
+		ft_strlcpy(start + varlen(vname) + 1, val, ft_strlen(val) + 1);
 	if (env_end != end)
-		shiftenvs(var(v, e, vlen(v)), e,
-			len - (end - var(v, e, vlen(v))), env_end);
+		shiftenvs(start, envp, len - (end - start), env_end);
 }
+
+// void	replace_var(char *v, char *val, char *e[])
+// {
+// 	int		len;
+// 	char	*dst;
+// 	char	*end;
+// 	char	*env_end;
+
+// 	len = 0;
+// 	end = var(v, e, vlen(v)) + ft_strlen(var(v, e, vlen(v)));
+// 	if (val != NULL)
+// 		len = vlen(v) + ft_strlen(val) + 1;
+// 	dst = var(v, e, vlen(v)) + len + 1;
+// 	if (ft_strchr(var(v, e, vlen(v)), '=') == NULL)
+// 		*end = '=';
+// 	if (val == NULL)
+// 		*(dst - 1) = '\0';
+// 	env_end = getenvmem_end(e);
+// 	if (env_end != end)
+// 		ft_memmove(dst, end + 1, env_end - end);
+// 	else
+// 		e[arr_len(e) - 2] = var(v, e, vlen(v));
+// 	if (dst - end - 1 < 0)
+// 		ft_bzero(env_end + (dst - end) - 1, end + 1 - dst);
+// 	if (val != NULL)
+// 		ft_strlcpy(var(v, e, vlen(v)) + vlen(v) + 1, val, ft_strlen(val) + 1);
+// 	if (env_end != end)
+// 		shiftenvs(var(v, e, vlen(v)), e,
+// 			len - (end - var(v, e, vlen(v))), env_end);
+// }
 
 void	unset_var(char *vname, char *envp[])
 {
--- sources/executor/quote_utils.c
+++ sources/executor/quote_utils.c
@@ -6,11 +6,108 @@
 /*   By: mlindenm <mlindenm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 22:04:43 by mrubina           #+#    #+#             */
-/*   Updated: 2023/10/17 08:30:57 by mlindenm         ###   ########.fr       */
+/*   Updated: 2023/10/17 12:45:42 by mlindenm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+// //returns a pointer to the first qote
+// static char	*firstquote(char *word)
+// {
+// 	char	*sq;
+// 	char	*dq;
+
+// 	sq = ft_strchr(word, '\'');
+// 	dq = ft_strchr(word, '"');
+// 	if (sq != NULL && dq != NULL)
+// 	{
+// 		if (sq < dq)
+// 			return (sq);
+// 		else
+// 			return (dq);
+// 	}
+// 	else if (sq != NULL)
+// 		return (sq);
+// 	else if (dq != NULL)
+// 		return (dq);
+// 	else
+// 		return (NULL);
+// }
+
+// // '"$USER"'
+// static void	exp_quotes(char **word, char *envp[], t_errdata *err)
+// {
+// 	char	*tmp;
+
+// 	if (*word != NULL && **word == '\'')
+// 	{
+// 		tmp = ft_strtrim(*word, "\'", err);
+// 		*word = tmp;
+// 	}
+// 	else if (*word != NULL && **word == '"')
+// 	{
+// 		tmp = ft_strtrim(*word, "\"", err);
+// 		*word = tmp;
+// 		varscan(word, envp, err);
+// 	}
+// 	else if (*word != NULL)
+// 		varscan(word, envp, err);
+// }
+
+// static char	*cropjoin2(char *str, char *qpart, int len, t_errdata *err)
+// {
+// 	char	*part1;
+// 	char	*rtn;
+
+// 	part1 = ft_substr(str, 0, len);
+// 	rtn = ft_strjoin(part1, qpart, err);
+// 	free(part1);
+// 	return (rtn);
+// }
+
+// static char	*cropjoin3(char *str, char *qpart, char *start, t_errdata *err)
+// {
+// 	char	*part1;
+// 	char	*rtn;
+// 	char	*part2;
+
+// 	part1 = ft_substr(str, 0, start - str);
+// 	part2 = ft_strchr(start, *start) + 1;
+// 	rtn = strjoin3(part1, qpart, part2, err);
+// 	free(part1);
+// 	return (rtn);
+// }
+
+// //replaces one pair of quotes
+// //returns null if no more qoutes left
+// //first quote should be found from the point where we left (next)
+// //4 cases: quote part equals word, in the middle
+// //on the right or on the left
+// int	replace_q(char **word, char *next, t_errdata *err)
+// {
+// 	char	*qpart;
+// 	char	*start;
+// 	char	*end;
+// 	char	*str;
+
+// 	str = *word;
+// 	start = firstquote(next);
+// 	if (start == NULL)
+// 		return (0);
+// 	end = ft_strchr(start, *start);
+// 	qpart = ft_substr(start, 0, end - start + 1);
+// 	exp_quotes(&qpart, err->envp, err);
+// 	if (start == str && *(end + 1) == '\0')
+// 		*word = qpart;
+// 	else if (start != str && *(end + 1) == '\0')
+// 		*word = cropjoin2(str, qpart, start - str, err);
+// 	else if (start == str)
+// 		*word = ft_strjoin(qpart, end + 1, err);
+// 	else if (*(end + 1) != '\0')
+// 		*word = cropjoin3(str, qpart, start, err);
+// 	return (ft_strlen(next) - (end - start) - 1);
+// }
 
 //returns a pointer to the first qote
 static char	*firstquote(char *word)
@@ -35,8 +132,13 @@ static char	*firstquote(char *word)
 		return (NULL);
 }
 
+static char	*closingquote(char *firstq, char q)
+{
+	return (ft_strchr(firstq + 1, q));
+}
+
 // '"$USER"'
-static void	exp_quotes(char **word, char *envp[], t_errdata *err)
+void	exp_quotes(char **word, char *envp[], t_errdata *err)
 {
 	char	*tmp;
 
@@ -55,26 +157,26 @@ static void	exp_quotes(char **word, char *envp[], t_errdata *err)
 		varscan(word, envp, err);
 }
 
-static char	*cropjoin2(char *str, char *qpart, int len, t_errdata *err)
+char	*cropjoin2(char *str, char *qpart, int len, t_errdata *err)
 {
 	char	*part1;
 	char	*rtn;
 
-	part1 = ft_substr(str, 0, len);
-	rtn = ft_strjoin(part1, qpart, err);
+	part1 = ft_substr(str, 0, len);//malloc
+	rtn = ft_strjoin(part1, qpart, err);//malloc
 	free(part1);
 	return (rtn);
 }
 
-static char	*cropjoin3(char *str, char *qpart, char *start, t_errdata *err)
+char	*cropjoin3(char *str, char *qpart, char *start, t_errdata *err)
 {
 	char	*part1;
 	char	*rtn;
 	char	*part2;
 
-	part1 = ft_substr(str, 0, start - str);
-	part2 = ft_strchr(start, *start) + 1;
-	rtn = strjoin3(part1, qpart, part2, err);
+	part1 = ft_substr(str, 0, start - str);//malloc
+	part2 = closingquote(start, *start) + 1;
+	rtn = strjoin3(part1, qpart, part2, err);//malloc
 	free(part1);
 	return (rtn);
 }
@@ -95,16 +197,16 @@ int	replace_q(char **word, char *next, t_errdata *err)
 	start = firstquote(next);
 	if (start == NULL)
 		return (0);
-	end = ft_strchr(start, *start);
-	qpart = ft_substr(start, 0, end - start + 1);
-	exp_quotes(&qpart, err->envp, err);
+	end = closingquote(start, *start);
+	qpart = ft_substr(start, 0, end - start + 1);//malloc
+	exp_quotes(&qpart, err->envp, err);//malloc
 	if (start == str && *(end + 1) == '\0')
 		*word = qpart;
 	else if (start != str && *(end + 1) == '\0')
-		*word = cropjoin2(str, qpart, start - str, err);
+		*word = cropjoin2(str, qpart, start - str, err);//malloc
 	else if (start == str)
-		*word = ft_strjoin(qpart, end + 1, err);
+		*word = ft_strjoin(qpart, end + 1, err);//malloc
 	else if (*(end + 1) != '\0')
-		*word = cropjoin3(str, qpart, start, err);
+		*word = cropjoin3(str, qpart, start, err);//malloc
 	return (ft_strlen(next) - (end - start) - 1);
 }
